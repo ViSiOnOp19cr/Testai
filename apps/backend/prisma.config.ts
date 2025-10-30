43aae0e9be508320205f7e539766e36c534a84e6
ViSiOnOp19cr <chandancr98195@gmail.com>
2025-10-31 00:09:51 +0530

    backend setup done

---
+++ apps/backend/prisma.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL || "";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: databaseUrl,
+  },
+});
