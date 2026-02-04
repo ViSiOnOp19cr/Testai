fbdb35bcccc9b1ccedc3406e8f3d9fe1b142d4c6
ViSiOnOp19cr <chandancr98195@gmail.com>
2026-02-04 23:56:22 +0530

    otp verification route done

--- apps/backend/src/controller/otp.controller.ts
+++ apps/backend/src/controller/otp.controller.ts
@@ -1,7 +1,7 @@
 import {Request ,Response} from 'express';
 import {checkRateLimit} from '../services/otpservice';
 import {generateOtp} from '../services/otpservice';
-import {storeOtp} from '../services/otpservice';
+import {storeOtp,verifyOtp} from '../services/otpservice';
 import {sendOtpEmail} from '../services/emailservice';
 
 export async function sendOtp(req: Request, res: Response) {
@@ -28,6 +28,30 @@ export async function sendOtp(req: Request, res: Response) {
         console.log("error in sendOtp",error);
         return res.status(500).json({
             message:"otp sent failed"
+        });
+    }
+}
+export const verifyotp = async(req:Request, res:Response)=>{
+    try{
+        const {email, otp, purpose='signup'} = req.body;
+        if(!email || !otp){
+            return res.status(400).json({
+                message:"email and otp both are required"
+            });
+        }
+        const result = await verifyOtp(email,otp,purpose);
+        if(!result.valid){
+            return res.status(400).json({
+                message:result.message
+            });
+        }
+        return res.status(200).json({
+            message:"OTP verified successfully."
+        });
+    }catch(error){
+        console.log("error in verifyotp",error);
+        return res.status(500).json({
+            message:"otp verification failed"
         });
     }
 }
--- apps/backend/src/routes/otp.routes.ts
+++ apps/backend/src/routes/otp.routes.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import {sendOtp} from '../controller/otp.controller';
+import {sendOtp,verifyotp} from '../controller/otp.controller';
 const router = express.Router();
 
 router.post('/send-otp',sendOtp);
+router.post('/verify-otp',verifyotp);
 
 export default router;
